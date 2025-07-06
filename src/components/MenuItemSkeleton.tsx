0d94b01b35c7f40f2975f1b675f101dd304c9025
Niborg <robin.caboche@epitech.eu>
2025-07-06 08:13:32 -0400

    feat(item card skeleton): item card skeleton finished

--- src/components/MenuItemSkeleton.tsx
+++ src/components/MenuItemSkeleton.tsx
@@ -8,9 +8,21 @@ const MenuItemSkeleton = ({ displayStyle }: MenuItemSkeletonProps) => {
   switch (displayStyle) {
     case "card":
       return (
-        <div className="bg-white rounded-xl shadow-md p-4 flex flex-col justify-between">
-          <Skeleton className="h-6 w-32 mb-2" />
-          <Skeleton className="h-4 w-24" />
+        <div className="bg-white rounded-xl shadow-md p-4 flex flex-col justify-between transition min-h-[7.5em]"
+        style={{
+            boxShadow: '0 1px 4px 0 rgba(0,0,0,0.07)',
+            borderColor: 'transparent',
+            outline: 'none'
+        }}>
+          <Skeleton className="h-7 w-32.5 overflow-hidden whitespace-nowrap relative" />
+          <div className="relative min-h-[2.5em] max-h-[2.5em]">
+            <div className="text-sm mt-1 select-none min-h-[2.5em] max-h-[2.5em]" style={{ color: "transparent", textDecoration: "none" }}>
+              &nbsp;
+            </div>
+          </div>
+          <div className="flex justify-end">
+            <Skeleton className="h-5.5 w-11 mt-2.5" />
+          </div>
         </div>
       );
     case "list":
